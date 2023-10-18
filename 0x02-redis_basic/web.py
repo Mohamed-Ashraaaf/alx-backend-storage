8d13605fe23bc8395f9af951ffc253c10e298b49
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-10-18 11:57:08 +0000

    redis advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
 """ Web page retrieval and caching """
 
-import requests
 import redis
+import http.client
 from typing import Callable
 
+# Initialize a Redis client
 redis_client = redis.Redis()
 
 
 def get_page(url: str) -> str:
     """ Retrieve HTML content from a URL and cache the result in Redis """
+    # Check if the URL is cached
     cached_content = redis_client.get(url)
 
     if cached_content is not None:
+        # If cached content exists, return it
         return cached_content.decode('utf-8')
 
-    response = requests.get(url)
+    try:
+        # Create an HTTP connection
+        conn = http.client.HTTPSConnection(url)
+        conn.request("GET", "/")
+        response = conn.getresponse()
 
-    if response.status_code == 200:
-        content = response.text
-        redis_client.setex(url, 10, content)
-        return content
-    else:
-        return f"Failed to fetch content from {url}"
+        if response.status == 200:
+            # If the request was successful
+            content = response.read().decode('utf-8')
+            redis_client.setex(url, 10, content)
+            return content
+        else:
+            # Handle non-200 status codes or request errors
+            return f"Failed to fetch from {url} (Status: {response.status})"
+    except Exception as e:
+        return f"Error while fetching content from {url}: {str(e)}"
 
 
 def count_calls(fn: Callable) -> Callable:
@@ -36,7 +47,8 @@ def count_calls(fn: Callable) -> Callable:
     return wrapper
 
 
+# Example usage
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/text"
+    url = "slowwly.robertomurray.co.uk"
     content = get_page(url)
     print(content)
